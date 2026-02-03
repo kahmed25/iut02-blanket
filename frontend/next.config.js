698fbbce77cd24dfa230d23d0e1ffb04a0086bb9
Rashed Ahmed <khahmed.rashed@gmail.com>
2026-02-03 14:20:03 -0800

    Simplify Amplify config - serve entire frontend directory

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
     domains: ['localhost', 'hz0qnbuf64.execute-api.ap-southeast-1.amazonaws.com', 'dev.d1js9a712g4lw.amplifyapp.com'],
     unoptimized: true
   },
-  // Don't use static export - it doesn't work well with dynamic client-side routes
-  // Amplify Hosting supports Next.js apps without static export
 }
 
 module.exports = nextConfig
